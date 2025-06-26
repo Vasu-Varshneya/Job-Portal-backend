02aaa03e5f60cb12867b178b7b698090a6452d15
Vasu-Varshneya <148642918+Vasu-Varshneya@users.noreply.github.com>
2025-06-26 14:42:35 +0530

    change to cors

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [`https://job-portal-frontend-lovat.vercel.app/`],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
