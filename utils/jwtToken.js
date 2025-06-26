f4b0aa40dd4f7036f8973fc952cc917ff5f2bca8
Vasu-Varshneya <148642918+Vasu-Varshneya@users.noreply.github.com>
2025-06-26 16:50:11 +0530

    cookie_updates

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,11 +10,19 @@ export const sendToken = (user, statusCode, res, message) => {
     sameSite: "None",        // ✅ Needed for cross-site cookies
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode)
+  .cookie("token", token, {
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: true,           // ✅ Required on HTTPS (Render)
+    sameSite: "None"        // ✅ Required for cross-origin
+  })
+  .json({
     success: true,
     user,
     message,
     token,
   });
+
 };
 
